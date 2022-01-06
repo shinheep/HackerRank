fb73d855e9306b50a036965437de9e06314544af
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2022-01-05 22:25:54 -0800

    Nested Lists problem solved

---
+++ python/nested_lists.py
@@ -0,0 +1,18 @@
+# Given the names and grades for each student in a class of N students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+# Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+
+Result =[]
+scorelist = []
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        Result+=[[name,score]]
+        scorelist+=[score]
+    b=sorted(list(set(scorelist)))[1] 
+
+    for a,c in sorted(Result):
+        if c==b:
+            print(a)
