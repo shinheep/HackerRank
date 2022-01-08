1c6a338c1438a5e80a034fb2a65bc9fc7515602f
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2022-01-08 10:14:14 -0800

    Split and Join problem solved

---
+++ python/split_and_join.py
@@ -0,0 +1,15 @@
+# Task
+# You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+# Function Description
+
+# Complete the split_and_join function in the editor below.
+
+# split_and_join has the following parameters:
+
+# string line: a string of space-separated words
+
+def split_and_join(line):
+    a = line.split(" ")
+    b = "-".join(a)
+    return(b)
