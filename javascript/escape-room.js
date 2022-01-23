c60e7e95fc983ec7d6673088b52b68413f9bf065
Shinhee Park <shinhee.park777@gmail.com>
2022-01-22 19:15:07 -0800

    Solved escape room

---
+++ javascript/escape-room.js
@@ -0,0 +1,31 @@
+const words = ['apple', 'pleas', 'please'];
+
+const keypads = [
+    'aelwxyz',
+    'aelpxyz',
+    'aelpsxy',
+    'saelprt',
+    'xaebksy'
+]
+
+const output = keypads.map(pad => {
+    const map = {};
+    pad.split('').forEach(char => map[char] = true);
+    const key = pad[0]
+
+    let sum = 0;
+    words
+        .filter(word => word.includes(key))
+        .forEach(word => {
+            const foundCharacters = word
+                .split('')
+                .reduce((acc, char) => acc + (map[char] ? 1 : 0), 0);
+            if (foundCharacters === word.length) {
+                sum++
+            }
+        });
+    
+        return sum;
+})
+
+console.log(output)
